653478927bc8ffa4ac3c1d814aba8240dcc195a8
0xodia <0xodia@solend.fi>
2023-06-16 17:27:20 -0400

    remove jsx files

--- src/components/SexyChameleonText/SexyChameleonText.jsx
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const SexyChameleonText = ({ children, className }) => {
-    const baseClass = 'text-white bg-clip-text';
-    const classes = [baseClass, className].join(' ');
-    return <span className={classes}>{children}</span>;
-};
-exports.default = SexyChameleonText;
